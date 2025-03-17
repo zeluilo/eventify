55f6341cbe4ebed6a9ba2b2fc93fd5d11d8e5197
Prince Zeluilo Cue-Eze <princezel18@gmail.com>
2025-03-17 19:46:50 +0000

    Commit

---
+++ pages/home.php
@@ -0,0 +1,11 @@
+<section id="hero" class="hero section dark-background">
+
+    <!-- <img src="assets/img/hero-bg.jpg" alt="Hero Background"> -->
+
+    <div class="hero-content">
+        <h2>Welcome to Eventify</h2>
+        <p>Your go-to platform for managing and showcasing exciting community events.</p>
+        <a href="#about" class="btn-get-started">Get Started</a>
+    </div>
+
+</section>
